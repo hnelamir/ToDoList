f6b8e0cc1f8be3d36060416148d53107d9d52209
HN_Linux <Hnelamir@gmail.com>
2026-02-21 22:52:58 +0200

    Organized the TaskController and adding some comments

--- src/main/java/com/SelfProject/ToDOList/controller/TaskController.java
+++ src/main/java/com/SelfProject/ToDOList/controller/TaskController.java
@@ -15,12 +15,12 @@ public class TaskController {
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
-
+    // 1. Revive All Tasks
     @GetMapping("/api/v1/tasks")
     public ResponseEntity<List<Task>> reviveAllTasks() {
         return ResponseEntity.ok(taskService.getAllTasks());
     }
-
+    // 2. Create Task
     @PostMapping("/api/v1/tasks")
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
         System.out.println("Received task: " + task);
@@ -28,20 +28,26 @@ public class TaskController {
         return ResponseEntity.ok(taskService.createTask(task));
 
     }
-
+    // 3. Delete Task
     @DeleteMapping("/api/v1/tasks/{id}")
     public void deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
     }
+
+    // 4.Update Task
     @PostMapping("/api/v1/tasks/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id,@RequestBody Task task) {
         task.setId(id);
         return ResponseEntity.ok(taskService.createTask(task));
     }
+
+    // 5.Toggle Task Status
     @GetMapping("/api/v1/tasks/{id}/toggle")
     public ResponseEntity<Task> toggleTaskStatus(@PathVariable Long id) {
         return ResponseEntity.ok(taskService.toggleTask(id));
     }
+
+
 
 
 
